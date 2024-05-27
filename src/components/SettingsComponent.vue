d2e6a7510e9a9a40208e66888d43491ce696d149
Yaz0612 <yazmin.acosta.jimenez@gmail.com>
2024-05-27 11:06:35 -0600

    Add settings

--- src/components/SettingsComponent.vue
+++ src/components/SettingsComponent.vue
@@ -1,15 +1,136 @@
 <template>
     <ion-page>
-        Settings
+        <ion-content>
+        <div>
+            <div class="container-set">
+                <div class="header-set">
+                    <ion-img src="engranaje.png" class="header-image-set"></ion-img>
+                    <ion-label class="text-set"><b>CONFIGURACIÓN</b></ion-label>
+                </div>
+            </div>
+            <div class="container-text-set">
+                <ion-icon :icon="personOutline" class="icon-account"></ion-icon>
+                <ion-label><b>Cuenta</b></ion-label>
+            </div>
+            <div class="divider-container">
+                <div class="divider"></div> 
+            </div>
+            <ion-list lines="none">
+                <ion-item aria-hidden="true" :icon="createOutline" routerLink="/editprofile">
+                    <ion-icon :icon="createOutline" class="icon-style"></ion-icon>
+                    <ion-label>Editar Perfil</ion-label>
+                    <ion-icon slot="end" :icon="chevronForwardOutline"></ion-icon>
+                </ion-item>
+                <ion-item aria-hidden="true" routerLink="/changepassword">
+                    <ion-icon :icon="lockOpenOutline" class="icon-style"></ion-icon>
+                    <ion-label>Cambiar Contraseña</ion-label>
+                    <ion-icon slot="end" :icon="chevronForwardOutline"></ion-icon>
+                </ion-item>
+                <ion-item aria-hidden="true" routerLink="/deleteaccount">
+                    <ion-icon :icon="closeCircleOutline" class="icon-style"></ion-icon>
+                    <ion-label>Eliminar cuenta</ion-label>
+                    <ion-icon slot="end" :icon="chevronForwardOutline"></ion-icon>
+                </ion-item>
+            </ion-list>
+        </div>
+    </ion-content>
     </ion-page>
 </template>
 
 <script>
-import {IonPage} from '@ionic/vue';
+import { IonPage, IonImg, IonLabel, IonIcon, IonItem, IonList,IonContent } from '@ionic/vue';
+import { lockOpenOutline, personOutline, createOutline, closeCircleOutline, chevronForwardOutline } from 'ionicons/icons';
+
 export default {
     name: 'SettingsComponent',
     components: {
         IonPage,
+        IonImg,
+        IonLabel,
+        IonIcon,
+        IonItem,
+        IonList,
+        IonContent,
+        Ion
+    },
+    setup() {
+        return {
+            personOutline,
+            lockOpenOutline,
+            createOutline,
+            closeCircleOutline,
+            chevronForwardOutline
+        };
     }
 }
 </script>
+
+<style>
+.container-set {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-end;
+    width: 100%;
+    height: 130px;
+    background: linear-gradient(to bottom, #BDD4DB, #FFFFFF);
+}
+
+.header-set {
+    position: absolute;
+    left: 50px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 35px;
+}
+
+.header-image-set {
+    width: 50px;
+    height: 50px;
+    margin-right: 15px;
+}
+
+.text-set {
+    font-size: 21px;
+}
+
+.container-text-set {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    margin-top: 40px;
+    padding: 15px 50px;
+    width: 100%;
+}
+
+.icon-account {
+    margin-right: 10px; 
+}
+
+.icon-style {
+    margin-right: 10px;
+    color: rgb(42, 121, 195);
+}
+
+.divider-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+
+.divider {
+    height: 1px;
+    width: 100%;
+    margin-left: 50px;
+    margin-right: 60px;
+    background-color: #757575;
+    margin-bottom: 20px;
+}
+
+ion-item { 
+    margin-left: 35px;
+    margin-right: 35px;
+    margin-bottom: 20px;
+}
+</style>
