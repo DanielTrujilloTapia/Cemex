a6c6c34bc564312a3d0ed94cf34623aabe7ed6f7
Jose Aldair U. Hdz. <josealdairugaldehernandez@gmail.com>
2024-05-27 13:56:56 -0600

    Add: Icons in TabsPage

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,18 +4,18 @@
       <ion-router-outlet :key="$route.fullPath"></ion-router-outlet>
       <ion-tab-bar>
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon aria-hidden="true" :icon="homeOutline" />
+          <ion-label>Inicio</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon aria-hidden="true" :icon="scanOutline" />
+          <ion-label>Escáner</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon aria-hidden="true" :icon="settingsOutline" />
+          <ion-label>Ajustes</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +24,5 @@
 
 <script setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { homeOutline, scanOutline, settingsOutline } from 'ionicons/icons';
 </script>
