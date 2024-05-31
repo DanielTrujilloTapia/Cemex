b279e347a29bd75e377ecc88620a45f0b8310bd8
Yaz0612 <yazmin.acosta.jimenez@gmail.com>
2024-05-31 12:05:53 -0600

    Update

--- src/components/EditProfileComponent.vue
+++ src/components/EditProfileComponent.vue
@@ -1,191 +1,319 @@
 <template>
-    <ion-page>
-      <ion-content>
-        <div class="container-editpro">
-          <div class="header-editpro">
-            <ion-img src="IEdit.png" class="header-image-editpro"></ion-img>
-            <ion-label class="text-editpro"><b>EDIT PROFILE</b></ion-label>
-          </div>
+  <ion-page>
+    <ion-content>
+      <div class="container-editpro">
+        <div class="header-editpro">
+          <ion-img src="IEdit.png" class="header-image-editpro"></ion-img>
+          <ion-label class="text-editpro"><b>EDIT PROFILE</b></ion-label>
         </div>
-        <div class="container-editphoto">
-          <ion-grid>
-            <ion-row>
-              <ion-col class="ion-text-center">
-                <div class="photo-container">
-                  <ion-img v-if="userPhoto" :src="userPhoto" class="photo-editpro" @click="selectAvatar"></ion-img>
-                  <div v-else class="photo-placeholder" @click="selectPhoto"></div>
-                  <div class="options">
-                    <ion-item lines="none" @click="selectPhoto">
+      </div>
+      <div class="container-editphoto">
+        <ion-grid>
+          <ion-row>
+            <ion-col class="ion-text-center">
+              <div class="photo-container">
+                <ion-img v-if="userPhoto" :src="userPhoto" class="photo-editpro" @click="selectPhoto"></ion-img>
+                <div v-else class="photo-placeholder" @click="selectPhoto"></div>
+                <div class="options">
+                  <ion-item lines="none">
                     <ion-icon :icon="createOutline" color="primary"></ion-icon>
-                    <ion-button size="small" fill="clear" @click="selectPhoto" :icon="createOutline">Editar</ion-button>
-                    </ion-item>
-                    <ion-item Lines="none" @click="deletePhoto">
-                        <ion-icon :icon="trashOutline" color="primary"></ion-icon>
-                    <ion-button size="small" fill="clear">Eliminar</ion-button>
-                    </ion-item>
-                  </div>
+                    <ion-button size="small" fill="clear" @click="selectPhoto">Editar</ion-button>
+                  </ion-item>
+                  <ion-item lines="none">
+                    <ion-icon :icon="trashOutline" color="primary"></ion-icon>
+                    <ion-button size="small" fill="clear" @click="deletePhoto">Eliminar</ion-button>
+                  </ion-item>
                 </div>
-              </ion-col>
-            </ion-row>
-          </ion-grid>
+              </div>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
+      <div class="container-data">
+        <div class="separation-data">
+          <ion-label>Usuario</ion-label>
+          <ion-input v-model="edusername" label-placement="floating" fill="outline" class="spacing" maxlength="10"></ion-input>
         </div>
-        <!-- <div class="data">
-            <ion-label>Usuario</ion-label>
-            <ion-input  label-placement="floating" fill="outline" maxlength="15"></ion-input>
-        </div> -->
-        <ion-button class="center">Guardar</ion-button>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script>
-  import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonImg, IonButton, IonAlert , IonItem,IonIcon , alertController} from '@ionic/vue';
-  import {trashOutline, createOutline} from 'ionicons/icons';
+      </div>
+      <div class="center">
+      <ion-button class="spacebuttons" @click="handleButtonCancel" fill="outline" color="danger">Cancelar</ion-button>
+      <ion-button class="spacebuttons" @click="saveUsername">Guardar</ion-button>
+    </div>
+      <ion-alert
+        :is-open="showAlert"
+        :header="alertHeader"
+        :message="alertMessage"
+        :buttons="alertButtons"
+        @didDismiss="showAlert = false">
+      </ion-alert>
+    </ion-content>
+  </ion-page>
+</template>
 
-  export default {
-    name: 'SettingsComponent',
-    components: {
-      IonPage,
-      IonContent,
-      IonGrid,
-      IonRow,
-      IonCol,
-      IonImg,
-      IonButton
-    },
-    setup(){
-        return{
-            trashOutline,
-            createOutline
-        }
-    },
-    data() {
-      return {
-        userPhoto: null
-      };
-    },
-    methods: {
-      async selectPhoto() {
-        const alert = await alertController.create({
-          header: 'Seleccionar imagen',
-          buttons: [
-            {
-              text: 'Tomar foto',
-              handler: () => {
-                this.takePhoto();
-              }
-            },
-            {
-              text: 'Seleccionar de galería',
-              handler: () => {
-                this.pickPhoto();
-              }
+<script>
+import { IonPage, IonContent, IonGrid, IonRow, IonCol, IonImg, IonButton, alertController, IonItem, IonIcon, IonAlert, IonInput, IonLabel } from '@ionic/vue';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { trashOutline, createOutline } from 'ionicons/icons';
+import '@ionic/pwa-elements';
+import {useRouter} from 'vue-router';
+
+export default {
+  name: 'SettingsComponent',
+  components: {
+    IonPage,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonButton,
+    IonItem,
+    IonIcon,
+    IonAlert,
+    IonInput,
+    IonLabel
+  },
+  setup() {
+    const router = useRouter();
+    const handleButtonCancel = () => {
+      router.push('/tabs/tab3');
+    }
+    return {
+      trashOutline,
+      createOutline,
+      handleButtonCancel
+    };
+  },
+  data() {
+    return {
+      userPhoto: null,
+      edusername: '',
+      originalUsername: '',
+      alertHeader: '',
+      alertMessage: '',
+      alertButtons: [],
+      showAlert: false,
+    };
+  },
+  async mounted() {
+    this.getUsername();
+  },
+  methods: {
+    async selectPhoto() {
+      const alert = await alertController.create({
+        header: 'Seleccionar imagen',
+        buttons: [
+          {
+            text: 'Tomar foto',
+            handler: () => {
+              this.takePhoto();
             }
-          ]
-        });
-        await alert.present();
-      },
-      async takePhoto() {
-        try {
+          },
+          {
+            text: 'Seleccionar de galería',
+            handler: () => {
+              this.pickPhoto();
+            }
+          }
+        ]
+      });
+      await alert.present();
+    },
+    async takePhoto() {
+      try {
+        const status = await Camera.checkPermissions();
+        if (status.camera === 'granted' || status.camera === 'prompt') {
           const capturedPhoto = await Camera.getPhoto({
             quality: 90,
             allowEditing: false,
-            resultType: CameraResultType.DataUrl
+            resultType: CameraResultType.DataUrl,
+            source: CameraSource.Camera
           });
           this.userPhoto = capturedPhoto.dataUrl;
-        } catch (error) {
-          console.error('Error al tomar la foto:', error);
+        } else {
+          console.error('Permiso de cámara denegado');
         }
-      },
-      async pickPhoto() {
-        try {
-          const galleryPhotos = await Camera.pickImages({ limit: 1 });
-          if (galleryPhotos.photos.length > 0) {
-            this.userPhoto = galleryPhotos.photos[0].webPath;
-          }
-        } catch (error) {
-          console.error('Error al seleccionar la imagen:', error);
-        }
-      },
-      deletePhoto() {
-        this.userPhoto = null;
+      } catch (error) {
+        console.error('Error al tomar la foto:', error);
       }
-    }
-  };
-  </script>
-  
-  <style>
-  .photo-editpro {
-    width: 100px;
-    height: 100px;
-    cursor: pointer;
+    },
+    async pickPhoto() {
+      try {
+        const galleryPhotos = await Camera.pickImages({ limit: 1 });
+        if (galleryPhotos.photos.length > 0) {
+          this.userPhoto = galleryPhotos.photos[0].webPath;
+        }
+        const alert = await alertController.getTop();
+        if (alert) {
+          await alert.dismiss();
+        }
+      } catch (error) {
+        console.error('Error al seleccionar la imagen:', error);
+      }
+    },
+    deletePhoto() {
+      this.userPhoto = null;
+    },
+    async getUsername() {
+      const userLogged = JSON.parse(localStorage.getItem('User-login'));
+      if (userLogged) {
+        this.edusername = userLogged.nom_usuario;
+        this.originalUsername = userLogged.nom_usuario; 
+      }
+    },
+    async saveUsername() {
+      const userLogged = JSON.parse(localStorage.getItem('User-login'));
+      if (!userLogged) {
+        this.showAlertDialog('Error', 'No se pudo obtener el usuario logueado');
+        return;
+      }
+
+      if (this.edusername === this.originalUsername) {
+        this.showAlertDialog('Info', 'No hay cambios en el nombre de usuario');
+        this.$router.push("/tabs/tab3");
+        return;
+      }
+
+      try {
+        const responseCheckUser = await fetch(`https://cemexapi20240515142245.azurewebsites.net/api/Usu_Usuarios?nom_usuario=${encodeURIComponent(this.edusername)}`);
+        if (!responseCheckUser.ok) {
+          this.showAlertDialog('Error', 'Error al verificar el usuario');
+          return;
+        }
+
+        const existingUsers = await responseCheckUser.json();
+        const userWithName = existingUsers.find(user => user.nom_usuario === this.edusername);
+        if (userWithName) {
+          this.showAlertDialog('Error', 'Ya existe un usuario con el mismo nombre, elige otro nombre de usuario');
+          return;
+        }
+
+        const response = await fetch(`https://cemexapi20240515142245.azurewebsites.net/api/Usu_Usuarios?id=${userLogged.id_usuario}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            id_usuario: userLogged.id_usuario,
+            nom_usuario: this.edusername,
+            contrasena: userLogged.contrasena,
+            idusucatestado: userLogged.idusucatestado,
+            idusucattipousuario: userLogged.idusucattipousuario
+          })
+        });
+        if (response.ok) {
+          this.showAlertDialog('Éxito', 'Nombre de usuario actualizado con éxito.');
+          this.$router.push("/tabs/tab3");
+        } else {
+          let errorData;
+          try {
+            errorData = await response.json();
+          } catch (e) {
+            errorData = { message: response.statusText };
+          }
+          this.showAlertDialog('Error', `Error al actualizar el nombre de usuario: ${errorData.message}`);
+        }
+      } catch (error) {
+        console.error("Error al cambiar el usuario:", error);
+        this.showAlertDialog('Error', 'Error al cambiar el nombre de usuario.');
+      }
+    },
+    showAlertDialog(header, message) {
+      this.alertHeader = header;
+      this.alertMessage = message;
+      this.alertButtons = [{ text: 'Ok', handler: () => { this.showAlert = false; } }];
+      this.showAlert = true;
+    },
   }
-  
-  .photo-placeholder {
-    width: 100px;
-    height: 100px;
-    background-color: rgb(236, 236, 236);
-    cursor: pointer;
-  }
-  .container-editpro {
-    position: relative;
+};
+</script>
+
+
+<style>
+.photo-editpro {
+width: 100px;
+height: 100px;
+cursor: pointer;
+object-fit: cover;
+}
+
+.photo-placeholder {
+width: 100px;
+height: 100px;
+background-color: rgb(236, 236, 236);
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+}
+
+.container-editpro {
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content: center;
+width: 100%;
+height: 130px;
+background: linear-gradient(to bottom, #BDD4DB, #FFFFFF);
+margin-bottom: 20px;
+}
+
+.container-editphoto {
+margin-left: 50px;
+margin-top: 30px;
+}
+
+.header-editpro {
+position: absolute;
+bottom: 0;
+left: 60px;
+display: flex;
+align-items: center;
+margin-bottom: 25px;
+}
+
+.header-image-editpro {
+width: 50px;
+height: 50px;
+margin-right: 15px;
+}
+
+.text-editpro {
+font-size: 20px;
+color: #000;
+}
+
+.photo-container {
+position: relative;
+display: flex;
+align-items: center;
+}
+
+.options {
+margin-left: 0px;
+display: flex;
+flex-direction: column;
+}
+
+.options ion-item {
+--inner-padding-end: 0px;
+}
+
+.center {
+display: flex;
+justify-content: center;
+}
+.spacebuttons{
+  margin:80px 15px
+}
+.container-data {
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    width: 100%;
-    height: 130px;
-    background: linear-gradient(to bottom, #BDD4DB, #FFFFFF);
-    margin-bottom: 20px;
-  }
-  .container-editphoto{
-    margin-left: 50px;
-    margin-top: 30px;
-  }
-  
-  .header-editpro {
-    position: absolute;
-    bottom: 0;
-    left: 60px;
-    display: flex;
-    align-items: center;
-    margin-bottom: 25px;
-  }
-  
-  .header-image-editpro {
-    width: 50px;
-    height: 50px;
-    margin-right: 15px;
-  }
-  
-  .text-editpro {
-    font-size: 20px;
-    color: #000;
-  }
-  
-  .photo-container {
-    position: relative;
-  }
-  
-  .options {
-    position: absolute;
-    top: 0;
-    right: 80px;
-  }
-  
-  .photo-options ion-button {
-    margin-left: 5px;
-  }
-  .center {
-    align-items: center;
-    margin-left: 150px;
-    margin-top: 200px; 
-  }
-  .data{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    margin-top: 10px;
+    margin-right: 60px;
+    margin-left: 60px;
+}
+
+.separation-data {
     margin-top: 50px;
-  }
-  </style>
-  
+}
+</style>
